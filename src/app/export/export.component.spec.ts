d39087ac566c86a2c540569a6785450cad0a91a7
Prerna <prerna@Prernas-MacBook-Air.local>
2025-02-26 13:43:09 +0000

    changing the format of the input from json to js

--- src/app/export/export.component.spec.ts
+++ src/app/export/export.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ExportComponent } from './export.component';
 import { MatIconModule } from '@angular/material/icon';
 
